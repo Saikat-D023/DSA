e1550290ad001670c6d9303687ae403d9a2b2930
Saikat-D023 <saikatdey358@gmail.com>
2025-02-07 22:54:07 +0530

    _

--- Palindrome.java
+++ Palindrome.java
@@ -2,17 +2,13 @@ public class Palindrome {
    
         static boolean checkPalindrome(int n) {
             int reverse = 0;
-          
-            // Copy of the original number so that the original
-            // number remains unchanged while finding the reverse
+            
             int temp = n;
             while (temp != 0) {
                 reverse = (reverse * 10) + (temp % 10);
                 temp = temp / 10;
             }
           
-            // If reverse is equal to the original number, the
-            // number is palindrome
             return (reverse == n);
         }
     
